5a05072c5a180bdeca3ea9921388f41f89694982
Daniel L <daniel.lee202@education.nsw.gov.au>
2026-05-20 10:36:01 +1000

    wip movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float sprintMultiplier = 1.5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private bool isSprinting;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float speed = isSprinting ? moveSpeed * sprintMultiplier : moveSpeed;
+        rb.linearVelocity = moveInput * speed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void Sprint(InputAction.CallbackContext context)
+    {
+        if (context.started) isSprinting = true;
+        if (context.canceled) isSprinting = false;
+    }
+}
